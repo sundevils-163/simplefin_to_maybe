fdd7702df3dd23a475c247db2e4aedd2d98a2d1f
steve <r3fund@gmail.com>
2025-03-24 23:11:22 -0500

    update various puts

--- bin/simplefin_to_maybe.rb
+++ bin/simplefin_to_maybe.rb
@@ -5,7 +5,7 @@ require_relative "../lib/simplefin_to_maybe"
 Dotenv.load(File.expand_path('../.env', __dir__))
 
 puts ""
-puts "Welcome to SimpleFINToMaybe CLI!"
+puts "Welcome to the SimpleFIN to Maybe transaction synchronizer!"
 puts ""
 
 # init a MaybeClient and get accounts for the family
@@ -15,18 +15,21 @@ maybe_client = SimpleFINToMaybe::MaybeClient.new
 family = make_selection(maybe_client.get_families, "name", "Please select your Maybe family instance:", auto_select_single: true)
 family_id = family.dig("id")
 return if family_id.nil?
+puts "Connected to family #{family.dig("name")} (#{family_id})"
 
 maybe_accounts = maybe_client.get_accounts(family_id)
 puts "Found #{maybe_accounts.length} Maybe account(s)!"
 return if 0 == maybe_accounts.length
 
 # init a SimpleFINClient and get all accounts
+puts ""
 puts "Connecting to SimpleFIN Bridge..."
 simplefin_client = SimpleFINToMaybe::SimpleFINClient.new
 
 simplefin_accounts = simplefin_client.get_all_accounts
 puts "Found #{simplefin_accounts.length} SimpleFIN account(s)!"
 return if 0 == simplefin_accounts.length
+puts "Beginning account enumeration..."
 
 # loop through all simplefin accounts
 if simplefin_accounts.is_a?(Array)
@@ -37,6 +40,7 @@ if simplefin_accounts.is_a?(Array)
     simplefin_name = simplefin_account.dig("name")
     simplefin_display_name = "#{simplefin_account.dig("org", "name")}: #{simplefin_name}"
 
+    puts ""
     puts "---"
     puts "Evaluating SimpleFIN account #{simplefin_display_name} (#{simplefin_account_uuid})"
 
@@ -48,7 +52,8 @@ if simplefin_accounts.is_a?(Array)
       
       unmatched_maybe_accounts = maybe_accounts.select { |account| account["import_id"].nil? }
       if unmatched_maybe_accounts.empty?
-        puts "All Maybe accounts have an associated SimpleFIN account; Create new accounts in Maybe to continue!"
+        puts ""
+        puts "All Maybe accounts have an associated SimpleFIN account; Create new accounts in Maybe to continue. Quitting!"
         break
       end
 
@@ -64,6 +69,7 @@ if simplefin_accounts.is_a?(Array)
     end
   
     # get all simplefin transactions for the calendar month
+    puts ""
     puts "Gathering transactions since #{get_first_of_month()}..."
     start_date_epoch = get_first_of_month(epoch: true)
     simplefin_transactions = simplefin_client.get_all_transactions(simplefin_account_uuid, start_date_epoch)
@@ -75,8 +81,11 @@ if simplefin_accounts.is_a?(Array)
     puts "Found #{existing_maybe_transactions.length} Maybe transaction(s) for this account!"
 
     # loop through all simplefin transactions
-    if simplefin_transactions.length != existing_maybe_transactions.length  # if they're the same, skip
+    if simplefin_transactions.length == existing_maybe_transactions.length  # if they're the same, skip
+      puts "Skipping to next account..."
+    else
       if simplefin_transactions.is_a?(Array)
+        puts "Inserting transaction(s)..."
         simplefin_transactions.each do |simplefin_transaction|
           transaction_id = simplefin_transaction.dig("id")
 
@@ -95,4 +104,5 @@ end
 
 # Close connection
 maybe_client.close
+puts ""
 puts "Finished synchronization!"
