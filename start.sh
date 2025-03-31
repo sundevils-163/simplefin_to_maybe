3f046e3fbb48f150a26247c777eed6e4a1c7ad79
steve <r3fund@gmail.com>
2025-03-31 12:23:15 -0500

    update good_job start

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ bundle exec rails db:seed
 # Start Rails and GoodJob
 echo "Starting Rails and GoodJob..."
 bundle exec rails server -b 0.0.0.0 -p 3000
-bundle exec good_job start --enable-cron
+bundle exec good_job start --execution-mode=async --enable-cron --max-threads=2
 
 #tail -f /dev/null
