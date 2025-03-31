2279d8b344f58656edf2d2e69fbb31d55212484a
steve <r3fund@gmail.com>
2025-03-31 12:24:15 -0500

    update port retrieval

--- app/jobs/run_all_syncs_job.rb
+++ app/jobs/run_all_syncs_job.rb
@@ -5,16 +5,24 @@ class RunAllSyncsJob < ApplicationJob
 
   queue_as :default
 
-  include Rails.application.routes.url_helpers  # Include the URL helpers module
-
   def perform(dont_schedule_followup = false)
 
     # Make the HTTP request
-    uri = URI.parse('http://localhost:3000/linkages/run_all_syncs')
+    port = ENV['PORT'] || '3000'
+    url = "http://localhost:#{port}/linkages/run_all_syncs"
+    Rails.logger.info "POST to #{url}}"
+    uri = URI.parse(url)
 
-    Net::HTTP.post(uri, {}.to_json, "Content-Type" => "application/json")
+    begin
+      response = Net::HTTP.post(uri, {}.to_json, { "Content-Type" => "application/json" })
+      
+      Rails.logger.info "HTTP Response Code: #{response.code}"
+      Rails.logger.info "HTTP Response Body: #{response.body}"
+    rescue => e
+      Rails.logger.error "HTTP Request failed: #{e.message}"
+    end
 
     # Schedule the next sync
-    update_cron_schedule unless dont_schedule_followup
+    update_cron_schedule unless dont_schedule_followup  #app/lib/cron_scheduler.rb
   end
 end
