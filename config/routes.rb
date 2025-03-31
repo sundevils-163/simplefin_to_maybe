7bb2190e9a9415e0bf1013ed07989ac6011bc671
steve <r3fund@gmail.com>
2025-03-31 15:50:12 -0500

    add database reset

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'test_maybe', to: 'settings#test_maybe'
   patch '/settings/:key', to: 'settings#update', as: 'update_setting'
 
+  post '/reset_database', to: 'database#reset'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
