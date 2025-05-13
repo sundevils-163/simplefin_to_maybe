d92b785c61e8fd024630b570d41000142e2cca5c
steve <r3fund@gmail.com>
2025-05-12 23:31:53 -0500

    handle all job types

--- app/lib/cron_scheduler.rb
+++ app/lib/cron_scheduler.rb
@@ -4,7 +4,8 @@ module CronScheduler
   require 'fugit'
   require 'tzinfo'
 
-  def update_cron_schedule
+  def update_cron_schedule(job_class)
+
     cron_expression = Setting.find_by(key: 'synchronization_schedule')&.value || '0 5,23 * * *'
   
     # Get the timezone from the environment, default to UTC
@@ -16,15 +17,15 @@ module CronScheduler
     next_run_time_local = timezone.utc_to_local(next_run_time_utc)
   
     # Remove any existing scheduled jobs that don't match this schedule
-    delete_unscheduled_jobs(next_run_time_local)
+    delete_unscheduled_jobs(job_class, next_run_time_local)
   
     # Schedule the job with ActiveJob
-    RunAllSyncsJob.set(wait_until: next_run_time_local).perform_later
+    job_class.set(wait_until: next_run_time_local).perform_later
   end
 
-  def delete_unscheduled_jobs(correct_time)
+  def delete_unscheduled_jobs(job_class, correct_time)
     GoodJob::Job
-      .where(queue_name: 'default', job_class: 'RunAllSyncsJob')
+      .where(queue_name: 'default', job_class: job_class.name)
       .where("scheduled_at > ?", Time.current)  # Only delete future jobs
       .where.not(scheduled_at: correct_time)
       .destroy_all
